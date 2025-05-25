6a44f2ecf089d38ff8265bdb969a8b4c9235dd97
acubeos <80309463+acubeos@users.noreply.github.com>
2025-05-25 18:41:45 +0100

    Style active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,4 +1,7 @@
+"use client"
+import classNames from "classnames"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 import React from "react"
 import { AiFillBug } from "react-icons/ai"
 
@@ -7,6 +10,7 @@ const NavBar = () => {
 		{ label: "Dashboard", href: "/" },
 		{ label: "Issues", href: "/issues" },
 	]
+	const currentPath = usePathname()
 	return (
 		<nav className='border-b flex gap-x-3 h-14 items-center px-3'>
 			<Link href='/'>
@@ -16,7 +20,11 @@ const NavBar = () => {
 				{links.map((link) => (
 					<li key={link.href}>
 						<Link
-							className='text-zinc-500 hover:text-zinc-800 transition-colors'
+							className={classNames({
+								"text-zinc-800": currentPath === link.href,
+								"text-zinc-500": currentPath !== link.href,
+								"hover: text-zinc-500 transition-colors font-bold": true,
+							})}
 							href={link.href}
 						>
 							{link.label}
