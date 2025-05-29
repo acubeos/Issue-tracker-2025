2bda87d472fd801aa0d371f4fe6a22dfed3bf494
acubeos <80309463+acubeos@users.noreply.github.com>
2025-05-29 19:43:01 +0100

    Add the edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,6 +1,7 @@
 import IssueStatusBadge from "@/app/components/IssueStatusBadge"
 import { prisma } from "@/prisma/client"
-import { Card, Flex, Heading, Text } from "@radix-ui/themes"
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes"
+import Link from "next/link"
 import { notFound } from "next/navigation"
 import Markdown from "react-markdown"
 interface Props {
@@ -17,16 +18,23 @@ const IssueDetailPage = async ({ params }: Props) => {
 	if (!issue) notFound()
 
 	return (
-		<div>
-			<Heading>{issue.title}</Heading>
-			<Flex gap='3' my='2'>
-				<IssueStatusBadge status={issue.status} />
-				<Text>{issue.updatedAt.toDateString()}</Text>
-			</Flex>
-			<Card className='prose lg:prose-xl' mt='4'>
-				<Markdown>{issue.description}</Markdown>
-			</Card>
-		</div>
+		<Grid columns={{ initial: "1", md: "2" }} gap='4'>
+			<Box>
+				<Heading>{issue.title}</Heading>
+				<Flex gap='3' my='2'>
+					<IssueStatusBadge status={issue.status} />
+					<Text>{issue.updatedAt.toDateString()}</Text>
+				</Flex>
+				<Card className='prose lg:prose-xl' mt='4'>
+					<Markdown>{issue.description}</Markdown>
+				</Card>
+			</Box>
+			<Box>
+				<Link href={`/issues/${issue.id}/edit`}>
+					<Button>Edit Issue</Button>
+				</Link>
+			</Box>
+		</Grid>
 	)
 }
 
