4df17940bc4e12639706c119044c44ae467bf6f0
acubeos <80309463+acubeos@users.noreply.github.com>
2025-05-31 18:57:50 +0100

    Handle error

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,38 +1,72 @@
+"use client"
 import { TrashIcon } from "@radix-ui/react-icons"
 import { AlertDialog, Button, Flex } from "@radix-ui/themes"
-import Link from "next/link"
+import axios from "axios"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
-	return (
-		<AlertDialog.Root>
-			<AlertDialog.Trigger>
-				<Button color='red'>
-					<TrashIcon />
-					{/* <Link as={"child"} href={`/issues/${issueId}/delete`}> */}
-					Delete Issue
-					{/* </Link> */}
-				</Button>
-			</AlertDialog.Trigger>
-			<AlertDialog.Content maxWidth='450px'>
-				<AlertDialog.Title>Delete issue</AlertDialog.Title>
-				<AlertDialog.Description size='2'>
-					Are you sure? This issue will no longer be accessible.
-				</AlertDialog.Description>
+	const router = useRouter()
+	const [error, setError] = useState(false)
 
-				<Flex gap='3' mt='4' justify='end'>
-					<AlertDialog.Cancel>
-						<Button variant='soft' color='gray'>
-							Cancel
-						</Button>
-					</AlertDialog.Cancel>
-					<AlertDialog.Action>
-						<Button variant='solid' color='red'>
-							Delete
-						</Button>
-					</AlertDialog.Action>
-				</Flex>
-			</AlertDialog.Content>
-		</AlertDialog.Root>
+	const deleteIsuue = async () => {
+		try {
+			await axios.delete(`/api/issues/${issueId}`)
+			router.push("/issues")
+			router.refresh()
+		} catch {
+			setError(true)
+		}
+	}
+
+	return (
+		<>
+			<AlertDialog.Root>
+				<AlertDialog.Trigger>
+					<Button color='red' onClick={deleteIsuue}>
+						<TrashIcon />
+						Delete Issue
+					</Button>
+				</AlertDialog.Trigger>
+				<AlertDialog.Content maxWidth='450px'>
+					<AlertDialog.Title>Delete issue</AlertDialog.Title>
+					<AlertDialog.Description size='2'>
+						Are you sure? This issue will no longer be accessible.
+					</AlertDialog.Description>
+
+					<Flex gap='3' mt='4' justify='end'>
+						<AlertDialog.Cancel>
+							<Button variant='soft' color='gray'>
+								Cancel
+							</Button>
+						</AlertDialog.Cancel>
+						<AlertDialog.Action>
+							<Button variant='solid' color='red'>
+								Delete
+							</Button>
+						</AlertDialog.Action>
+					</Flex>
+				</AlertDialog.Content>
+			</AlertDialog.Root>
+
+			<AlertDialog.Root open={error}>
+				<AlertDialog.Content maxWidth='450px'>
+					<AlertDialog.Title color='red'>Error</AlertDialog.Title>
+					<AlertDialog.Description size='2'>
+						This issue could not be deleted
+					</AlertDialog.Description>
+
+					<Button
+						color='gray'
+						variant='soft'
+						mt='2'
+						onClick={() => setError(false)}
+					>
+						Ok
+					</Button>
+				</AlertDialog.Content>
+			</AlertDialog.Root>
+		</>
 	)
 }
 
