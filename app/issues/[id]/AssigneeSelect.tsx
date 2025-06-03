b6e8a4e4dfe1d657fd8f660d88841608006ce681
acubeos <80309463+acubeos@users.noreply.github.com>
2025-06-03 21:08:31 +0100

    Refactor: Assignee component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -3,44 +3,52 @@ import { Issue, User } from "@/app/generated/prisma"
 import { Select, Skeleton } from "@radix-ui/themes"
 import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
+import toast, { Toaster } from "react-hot-toast"
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-	const {
-		data: users,
-		error,
-		isLoading,
-	} = useQuery<User[]>({
+	const { data: users, error, isLoading } = useUsers()
+
+	if (error) return null
+
+	if (isLoading) return <Skeleton height='2rem' />
+
+	const assignedIssue = (userId: string) => {
+		axios
+			.patch(`/api/issues/${issue.id}`, {
+				assignedToUserId: userId === "unassigned" ? null : userId,
+			})
+			.catch(() => toast.error("Changes could not be saved."))
+	}
+	return (
+		<>
+			<Select.Root
+				defaultValue={issue.assignedToUserId || "unassigned"}
+				onValueChange={assignedIssue}
+			>
+				<Select.Trigger placeholder='Assign...' />
+				<Select.Content>
+					<Select.Group>
+						<Select.Label>Suggestions</Select.Label>
+						<Select.Item value='unassigned'>Unassigned</Select.Item>
+						{users?.map((user) => (
+							<Select.Item key={user.id} value={user.id}>
+								{user.name}
+							</Select.Item>
+						))}
+					</Select.Group>
+				</Select.Content>
+			</Select.Root>
+			<Toaster />
+		</>
+	)
+}
+
+const useUsers = () =>
+	useQuery<User[]>({
 		queryKey: ["users"],
 		queryFn: () => axios.get("/api/users").then((res) => res.data),
 		staleTime: 60 * 1000,
 		retry: 3,
 	})
-	if (error) return null
-
-	if (isLoading) return <Skeleton height='2rem' />
-	return (
-		<Select.Root
-			defaultValue={issue.assignedToUserId || "Unassigned"}
-			onValueChange={(userId) => {
-				axios.patch(`/api/issues/${issue.id}`, {
-					assignedToUserId: userId === "unassigned" ? null : userId,
-				})
-			}}
-		>
-			<Select.Trigger placeholder='Assign...' />
-			<Select.Content>
-				<Select.Group>
-					<Select.Label>Suggestions</Select.Label>
-					<Select.Item value='unassigned'>Unassigned</Select.Item>
-					{users?.map((user) => (
-						<Select.Item key={user.id} value={user.id}>
-							{user.name}
-						</Select.Item>
-					))}
-				</Select.Group>
-			</Select.Content>
-		</Select.Root>
-	)
-}
 
 export default AssigneeSelect
