f9fe101e4df38412a9ca159e301bbb45b58f7554
acubeos <80309463+acubeos@users.noreply.github.com>
2025-06-09 18:37:55 +0100

    Build the issue summary component

---
+++ app/IssueSummary.tsx
@@ -0,0 +1,38 @@
+import { Card, Flex, Text } from "@radix-ui/themes"
+import { Status } from "./generated/prisma"
+import Link from "next/link"
+
+interface Props {
+	open: number
+	closed: number
+	inProgress: number
+}
+
+const IssueSummary = ({ open, closed, inProgress }: Props) => {
+	const containers: { label: string; value: number; status: Status }[] = [
+		{ label: "Open Issues", value: open, status: "OPEN" },
+		{ label: "In Progress Issues", value: inProgress, status: "IN_PROGRESS" },
+		{ label: "Closed Issues", value: closed, status: "CLOSED" },
+	]
+	return (
+		<Flex gap='3'>
+			{containers.map((container) => (
+				<Card key={container.status}>
+					<Flex direction='column' gap='2'>
+						<Link
+							className='text-sm font-medium'
+							href={`/issues?status=${container.status}`}
+						>
+							{container.label}
+						</Link>
+						<Text size='5' weight='bold'>
+							{container.value}
+						</Text>
+					</Flex>
+				</Card>
+			))}
+		</Flex>
+	)
+}
+
+export default IssueSummary
